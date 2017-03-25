c2bf6d1ded71c9cac9d0b8a79c0276fe669be9f7
liuyuexclusive <453017973@qq.com>
2017-03-25 17:19:36 +0800

    项目结构调整

---
+++ LY.Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LY.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>() { };
+            var xx = list.AsQueryable();
+            Console.WriteLine(xx.ElementType);
+            Console.WriteLine(xx.Expression);
+            Console.WriteLine(xx.Provider);
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+
+    public class MyQueryAble : IQueryable
+    {
+        public Type ElementType => throw new NotImplementedException();
+
+        public Expression Expression => throw new NotImplementedException();
+
+        public IQueryProvider Provider => throw new NotImplementedException();
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

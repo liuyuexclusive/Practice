c78822009e8c234ca95a8dd50d99cd7e759406fa
liuyuexclusive <453017973@qq.com>
2018-12-10 11:53:44 +0800

    update autostart

--- LY.AutoStart/Program.cs
+++ LY.AutoStart/Program.cs
@@ -12,32 +12,88 @@ namespace LY.AutoStart
     {
         static void Main(string[] args)
         {
-            if (args == null || args.Length == 0)
+            try
             {
-                args = new string[] { "practice" };
-            }
+                if (args == null || args.Length == 0)
+                {
+                    args = new string[] { "practice" };
+                }
 
+                if (args == null || args.Length <= 0)
+                {
+                    Console.WriteLine("请输入workspace名称");
+                    return;
+                }
+                var workspace = new DirectoryInfo(Directory.GetCurrentDirectory());
+                var root = workspace.Root;
+                while (true)
+                {
+                    if (workspace.FullName == root.FullName)
+                    {
+                        Console.WriteLine($"无法找到workspace{args[0]}");
+                        return;
+                    }
+                    if (workspace.Name == args[0])
+                    {
+                        break;
+                    }
+                    workspace = workspace.Parent;
+                }
+                var targets = new List<DirectoryInfo>() { };
+                var gateway = workspace.GetDirectories().FirstOrDefault(x => x.Name.EndsWith("Gateway"));
+                if (gateway != null)
+                {
+                    targets.Add(gateway);
+                }
+                targets.AddRange(workspace.GetDirectories().Where(x => x.Name.EndsWith("Service")));
+                KillDotnet();
 
-            if (args == null || args.Length <= 0)
-            {
-                Console.WriteLine("请输入workspace名称");
-                return;
+                foreach (var dic in targets)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine($"cd {dic.FullName}");
+                    sb.AppendLine($"dotnet restore");
+                    sb.AppendLine($"dotnet build");
+                    sb.AppendLine($"dotnet publish -c Release -o D:\\MyFiles\\Code\\practicePublish\\{dic.Name}");
+                    sb.AppendLine($"exit");
+                    using (FileStream fs = File.Create($"{dic.Name}_build.bat"))
+                    {
+                        StreamWriter sw = new StreamWriter(fs);
+                        sw.WriteLine(sb.ToString());
+                        sw.Flush();
+                    }
+                    using (Process process = new Process())
+                    {
+                        process.StartInfo.FileName = Path.Combine(Directory.GetCurrentDirectory(), $"{dic.Name}_build.bat");
+                        process.Start();
+                        process.WaitForExit();
+                    }
+                }
+                //kill
+                KillDotnet();
+                foreach (var dic in targets)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine($"cd D:\\MyFiles\\Code\\practicePublish\\{dic.Name}");
+                    sb.AppendLine($"dotnet {dic.Name}.dll");
+                    using (FileStream fs = File.Create($"{dic.Name}_run.bat"))
+                    {
+                        StreamWriter sw = new StreamWriter(fs);
+                        sw.WriteLine(sb.ToString());
+                        sw.Flush();
+                    }
+                    Process.Start(Path.Combine(Directory.GetCurrentDirectory(), $"{dic.Name}_run.bat"));
+                }
             }
-            var workspace = $"C:\\Program Files (x86)\\Jenkins\\workspace\\{args[0]}";
-            if (!Directory.Exists(workspace))
+            catch (Exception ex)
             {
-                Console.WriteLine($"无法找到workspace{workspace}");
-                return;
+                KillDotnet();
+                Console.WriteLine(ex.ToString());
             }
-            var dirWorkspace = new DirectoryInfo(workspace);
-            var targets = new List<DirectoryInfo>() { };
-            var gateway = dirWorkspace.GetDirectories().FirstOrDefault(x => x.Name.EndsWith("Gateway"));
-            if (gateway != null)
-            {
-                targets.Add(gateway);
-            }
-            targets.AddRange(dirWorkspace.GetDirectories().Where(x => x.Name.EndsWith("Service")));
+        }
 
+        private static void KillDotnet()
+        {
             //kill
             foreach (var existProcess in Process.GetProcessesByName("dotnet"))
             {
@@ -46,24 +102,6 @@ namespace LY.AutoStart
                     existProcess.Kill();
                 }
             }
-
-            foreach (var dic in targets)
-            {
-                StringBuilder sb = new StringBuilder();
-                sb.AppendLine($"cd {dic.FullName}");
-                sb.AppendLine($"dotnet restore");
-                sb.AppendLine($"dotnet build");
-                sb.AppendLine($"dotnet publish -c Release -o D:\\MyFiles\\Code\\practicePublish\\{dic.Name}");
-                sb.AppendLine($"cd D:\\MyFiles\\Code\\practicePublish\\{dic.Name}");
-                sb.AppendLine($"dotnet D:\\MyFiles\\Code\\practicePublish\\{dic.Name}\\{dic.Name}.dll");
-                using (FileStream fs = File.Create($"{dic.Name}.bat"))
-                {
-                    StreamWriter sw = new StreamWriter(fs);
-                    sw.WriteLine(sb.ToString());
-                    sw.Flush();
-                }
-                Process.Start(Path.Combine(Directory.GetCurrentDirectory(), $"{dic.Name}.bat"));
-            }    
         }
     }
 }

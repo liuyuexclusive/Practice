e8c244ff2e37851e99d2aaf7af881e4c954a0d8b
liuyuexclusive <453017973@qq.com>
2019-03-08 14:02:21 +0800

    fix bug

---
+++ LY.Application/Api/SysApi.cs
@@ -0,0 +1,17 @@
+﻿using LY.Common.API;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient;
+using WebApiClient.Attributes;
+
+namespace LY.Application.Api
+{
+    [HttpHost("http://localhost:9000")]
+    public interface ISysApi : IHttpApi
+    {
+        [HttpGet("/Schedule/GetList")]
+        Task<OutputList<object>> GetList();            
+    }    
+}
